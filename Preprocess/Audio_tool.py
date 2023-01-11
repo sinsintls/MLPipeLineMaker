4bea59a0ff02dd65d3472f45f28ada113523a08f
dkshin <sinsintls123@gmail.com>
2023-01-11 10:26:23 +0900

    add crop pad

--- Preprocess/Audio_tool.py
+++ Preprocess/Audio_tool.py
@@ -3,6 +3,54 @@ from typing import Dict
 import librosa
 import numpy as np
 
+#######################################
+class Params:
+    max_length = "max_length"
+    padding_mode = "padding_mode"
+    repeat = "repeat"
+    zero = "zero"
+
+#######################################
+
+def crop_n_pad(data: np.ndarray, params: Dict=None):
+
+    max_len = params[Params.max_length]
+    padding_mode = params[Params.padding_mode]
+    data_len = len(data)
+
+    if max_len < data_len:
+
+        pp_data = data[:len]
+
+    elif max_len > data_len:
+
+        pp_data = []
+
+        if max_len < data_len:
+
+            pp_data = data[:len]
+
+        elif max_len > data_len:
+
+            pp_data = [data]
+            rest = max_len % data_len
+
+            if rest != 0 and padding_mode == "repeat":
+
+                num_iter = int(max_len / data_len) - 1
+
+                for i in range(num_iter):
+                    pp_data.append(data)
+
+                pp_data.append(data[:rest])
+
+            elif rest != 0 and padding_mode == "zero":
+
+                pp_data.append(np.zeros(max_len - data_len))
+
+            pp_data = np.hstack(pp_data)
+
+    return pp_data
 
 def resample(data: np.ndarray, params: Dict=None):
     return librosa.resample(y=data, **params)
